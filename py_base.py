c8f30cbc9c53667fc6d7b85bc2ef9558f1c5c244
madao <83685076@qq.com>
2018-01-24 13:00:28 +0800

    py/18/1/24

--- py_base.py
+++ py_base.py
@@ -496,3 +496,434 @@ def is_email(addr):
 # danci(str)
 
 #线程进程
+# 内置模块 datatime
+from datetime import datetime,timedelta
+dt=datetime(2015,4,19,12,20)
+print(dt)
+
+#str转换成datetime
+cday=datetime.strptime('2018-1-22 21:21:21','%Y-%m-%d %H:%M:%S')
+print(cday)
+#datetime转换成str
+now=datetime.now()
+print(now+timedelta(days=2, hours=12))
+print(now.strftime('%a,%b %d %H:%m'))
+
+# collections
+# namedtuple 用属性不是索引  相对于class来说简单
+from collections import namedtuple
+Point=namedtuple('Point',['x','y'])
+p=Point(1,5)
+print(p.x,p.y)
+
+# deque 双向列表  appendleft()和popleft()
+from collections import deque
+q=deque(['s','f','v','w'])
+q.append('e')
+q.appendleft('a')
+print(q)
+
+# defaultdict  key不存在时返回默认值
+from collections import defaultdict
+dd=defaultdict(lambda:'N/A')
+dd['key1']='abc'
+print(dd['key1'],dd['key2'])
+
+# OrderedDict 按照插入的顺序 有序
+from collections import OrderedDict
+d = dict([('a', 1), ('o', 2), ('c', 3)])
+od = OrderedDict([('a', 1), ('o', 2), ('c', 3)])
+print(d,od)
+
+# FIFO
+from collections import OrderedDict
+class LastUpdatedOrderedDict(OrderedDict):
+
+    def __init__(self, capacity):
+        super(LastUpdatedOrderedDict, self).__init__()
+        self._capacity = capacity
+
+    def __setitem__(self, key, value):
+        containsKey = 1 if key in self else 0
+        if len(self) - containsKey >= self._capacity:
+            last = self.popitem(last=False)
+            print('remove:', last)
+        if containsKey:
+            del self[key]
+            print('set:', (key, value))
+        else:
+            print('add:', (key, value))
+        OrderedDict.__setitem__(self, key, value)
+
+
+# Counter  dict子类
+from collections import Counter
+c=Counter()
+for ch in 'sdjhhscujnsujn':
+	c[ch]+=1
+
+print(c)
+
+
+# base64
+# Base64是一种任意二进制到文本字符串的编码方法，常用于在URL、Cookie、网页中传输少量二进制数据。
+
+# struct模块来解决bytes和其他二进制数据类型的转换
+import struct
+print(struct.pack('>I',10240099))
+print(struct.unpack('>IH', b'\xf0\xf0\xf0\xf0\x80\x80'))
+
+# hashlib  摘要算法
+import hashlib
+md5=hashlib.md5()
+md5.update('how to use md5 in python hashlib?'.encode('utf-8'))
+print(md5.hexdigest())
+
+# 验证口令
+# import hashlib
+# def login(user, password):
+#     md5=hashlib.md5()
+#     md5.update(password.encode('utf-8'))
+#     return md5.hexdigest()==db[user]
+
+sha1 = hashlib.sha1()
+sha1.update('how to use sha1 in '.encode('utf-8'))
+sha1.update('python hashlib?'.encode('utf-8'))
+print(sha1.hexdigest())
+
+# hmac 算法 在md5算法基础上
+
+
+# itertools
+import itertools 
+natuals=itertools.count(1)
+# 无限的
+# for n in natuals:
+# 	print(n)
+ns = itertools.takewhile(lambda x: x <= 10, natuals)
+print(ns)
+print(list(ns))
+
+cs=itertools.cycle('ABC')
+ns = itertools.repeat('A', 3)
+
+
+
+# chain() 串联
+for c in itertools.chain('ABC','XZY'):
+	print(c)
+
+
+# groupby()把迭代器中相邻的重复元素挑出来放在一起：  忽略大小写用lambda
+for key,group in itertools.groupby('AasdsdsfaassDSAD',lambda c:c.upper()):
+	print(key,list(group))
+
+
+# contextlib  任何实现了 __enter__() 和 __exit__() 方法的对象都可称之为上下文管理器
+# 只要正确实现了上下文管理，就可以用于with语句。自动close()
+class Query(object):
+	def __init__(self,name):
+		self.name=name
+	def __enter__(self):
+		print('Begin')
+		return self
+	def __exit__(self,exc_type,exc_value,traceback):
+		if exc_type:
+			print('Error')
+		else:
+			print('End')
+	def query(self):
+		print('Query info about %s...' % self.name)
+
+with Query('Bob') as q:
+	q.query()
+
+# 用contextmanager
+# yield 之前的语句在 __enter__ 方法中执行，yield 之后的语句在 __exit__ 方法中执行
+from contextlib import contextmanager
+
+class Query(object):
+	def __init__(self,name):
+		self.name=name
+	def query(self):
+		print('Query info about %s...' % self.name)
+
+@contextmanager
+def create_query(name):
+	print('Begin')
+	q=Query(name)
+	yield q
+	print('End')
+
+with create_query('Bob') as q:
+	q.query()
+
+# 我们希望在某段代码执行前后自动执行特定代码，也可以用@contextmanager实现
+@contextmanager
+def tag(name):
+    print("<%s>" % name)
+    yield
+    print("</%s>" % name)
+
+with tag("h1"):
+    print("hello")
+    print("world")
+
+
+# closing()来把该对象变为上下文对象
+from contextlib import closing
+from urllib.request import urlopen
+
+# with closing(urlopen('https://www.python.org')) as page:
+#     for line in page:
+#         print(line)
+
+@contextmanager
+def closing(thing):
+    try:
+        yield thing
+    finally:
+        thing.close()
+
+
+# urllib提供的功能就是利用程序去执行各种HTTP请求。如果要模拟浏览器完成特定功能，需要把请求伪装成浏览器。伪装的方法是先监控浏览器发出的请求，再根据浏览器的请求头来伪装，User-Agent头就是用来标识浏览器的。
+# urllib
+# from urllib import request
+# with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+# 	data=f.read()
+# 	print('status:',f.status,f.reason)
+# 	for k,v in f.getheaders():
+# 		print('%s:%s' %(k,v))
+# 	print('Data:',data.decode('utf-8'))
+
+# 模仿iPhone6去获取 
+# from urllib import request
+# req = request.Request('http://www.douban.com/')
+# req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+# with request.urlopen(req) as f:
+#     print('Status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s: %s' % (k, v))
+#     print('Data:', f.read().decode('utf-8'))
+
+
+# 模拟微博登录
+# from urllib import request, parse
+
+# print('Login to weibo.cn...')
+# email = input('Email: ')
+# passwd = input('Password: ')
+# login_data = parse.urlencode([
+#     ('username', email),
+#     ('password', passwd),
+#     ('entry', 'mweibo'),
+#     ('client_id', ''),
+#     ('savestate', '1'),
+#     ('ec', ''),
+#     ('pagerefer', 'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F')
+# ])
+
+# req = request.Request('https://passport.weibo.cn/sso/login')
+# req.add_header('Origin', 'https://passport.weibo.cn')
+# req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+# req.add_header('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
+
+# with request.urlopen(req, data=login_data.encode('utf-8')) as f:
+#     print('Status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s: %s' % (k, v))
+#     print('Data:', f.read().decode('utf-8'))
+
+
+# ProxyHandler 处理
+# proxy_handler = urllib.request.ProxyHandler({'http': 'http://www.example.com:3128/'})
+# proxy_auth_handler = urllib.request.ProxyBasicAuthHandler()
+# proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
+# opener = urllib.request.build_opener(proxy_handler, proxy_auth_handler)
+# with opener.open('http://www.example.com/login.html') as f:
+#     pass
+
+
+# XML
+# from xml.parsers.expat import ParserCreate
+
+# class DefaultSaxHandler(object):
+#     def start_element(self, name, attrs):
+#         print('sax:start_element: %s, attrs: %s' % (name, str(attrs)))
+
+#     def end_element(self, name):
+#         print('sax:end_element: %s' % name)
+
+#     def char_data(self, text):
+#         print('sax:char_data: %s' % text)
+
+# xml = r'''<?xml version="1.0"?>
+# <ol>
+#     <li><a href="/python">Python</a></li>
+#     <li><a href="/ruby">Ruby</a></li>
+# </ol>
+# '''
+
+# handler = DefaultSaxHandler()
+# parser = ParserCreate()
+# parser.StartElementHandler = handler.start_element
+# parser.EndElementHandler = handler.end_element
+# parser.CharacterDataHandler = handler.char_data
+# parser.Parse(xml)
+
+# HTMLParser
+#定义一个MyParser继承自HTMLParser
+from html.parser import HTMLParser
+
+class MyParser(HTMLParser):
+    re=[]#放置结果
+    flg=0#标志，用以标记是否找到我们需要的标签
+    def handle_starttag(self, tag, attrs):
+        if tag=='h3':#目标标签
+            for attr in attrs:
+                if attr[0]=='class' and attr[1]=='tb-main-title':#目标标签具有的属性
+                    self.flg=1#符合条件则将标志设置为1
+                    break
+        else:
+            pass
+  
+    def handle_data(self, data):
+        if self.flg==1:
+            self.re.append(data.strip())#如果标志为我们需要的标志，则将数据添加到列表中
+            self.flg=0#重置标志，进行下次迭代
+        else:
+            pass
+  
+# my=MyParser()
+# my.feed('https://api.douban.com/v2/book/2129650')
+
+
+
+# 数据库mysql
+# import mysql.connector
+
+# conn = mysql.connector.connect(user='root', password='479400', database='local_paul_db')
+# cursor = conn.cursor()
+# cursor.execute('create table user(id int auto_increment,name varchar(10),age int,primary key(id))ENGINE=InnoDB')
+# cursor.execute('insert into user(name,age) values(%s,%s)', ['paul', '28'])
+# print("cursor.rowcount:", cursor.rowcount)
+
+# conn.commit()
+# cursor.close()
+
+# cur = conn.cursor()
+# cur.execute('select * from user where name = %s', ['paul', ])
+# print("cur.fetchall():", cur.fetchall())
+# cur.close()
+# conn.close()
+
+
+
+# Anaconda 第三方模块  Pillow图像处理
+from PIL import Image
+
+# 打开一个jpg图像文件，注意是当前路径:
+im = Image.open('test.jpg')
+# 获得图像尺寸:
+w, h = im.size
+print('Original image size: %sx%s' % (w, h))
+# 缩放到50%:
+im.thumbnail((w//2, h//2))
+print('Resize image to: %sx%s' % (w//2, h//2))
+# 把缩放后的图像用jpeg格式保存:
+# im.save('thumbnail.jpg', 'jpeg')
+
+from PIL import Image, ImageFilter
+
+# 打开一个jpg图像文件，注意是当前路径:
+im = Image.open('test.jpg')
+# 应用模糊滤镜:
+im2 = im.filter(ImageFilter.BLUR)
+# im2.save('blur.jpg', 'jpeg')
+
+
+# 生成验证码
+from PIL import Image,ImageDraw,ImageFilter,ImageFont
+import random
+
+#随机字母
+def rndChar():
+	return chr(random.randint(65,90))
+
+def rndColor():
+	return (random.randint(64,255),random.randint(64,255),random.randint(64,255))
+
+def rndColor2():
+	return (random.randint(32,127),random.randint(32,127),random.randint(32,127))
+
+width=60*4
+height=60
+image=Image.new('RGB',(width,height),(255,255,255))
+
+font=ImageFont.truetype('Arial.ttf',36)
+draw=ImageDraw.Draw(image)
+# 填充每个像素
+for x in range(width):
+	for y in range(height):
+		draw.point((x,y),fill=rndColor())
+# 输出文字
+for t in range(4):
+	draw.text((60*t+10,10),rndChar(),font=font,fill=rndColor2())
+
+image = image.filter(ImageFilter.BLUR)
+# image.save('code.jpg', 'jpeg')
+
+
+# requests
+import requests
+r = requests.get('https://www.douban.com/search', params={'q': 'python', 'cat': '1001'})
+print(r.status_code) #r.text
+# content属性获得bytes对象
+# print(r.content)
+
+# params = {'key': 'value'}
+# r = requests.post(url, json=params) # 内部自动序列化为JSON
+
+
+# chardet 检测编码  不知道为什么不支持
+# chardet.detect(b'Hello, world!')
+
+
+# psutil 编写脚本简化日常的运维工作
+# process and system utilities 硬件os网络
+import psutil
+print(psutil.cpu_count(logical=False),psutil.cpu_times())
+
+for x in range(10):
+	psutil.cpu_percent(interval=1, percpu=True)
+
+
+# virtualenv就是用来为一个应用创建一套“隔离”的Python运行环境。
+
+
+# 图形化界面GUI Tkinter
+# 每个Button、Label、输入框等，都是一个Widget。Frame则是可以容纳其他Widget的Widget，所有的Widget组合起来就是一棵树。
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)# super
+        self.pack()  # pack()方法把Widget加入到父容器中，并实现布局
+        self.createWidgets()
+
+    def createWidgets(self):
+    	self.nameInput=Entry(self)
+    	self.nameInput.pack()
+    	self.alertButton=Button(self,text='Hello',command=self.hello)
+    	self.alertButton.pack()
+    	
+    def hello(self):
+        name=self.nameInput.get() or 'world'
+        messagebox.showinfo('Message','Hello,%s' %name)
+
+app = Application()
+# 设置窗口标题:
+app.master.title('Hello World')
+# 主消息循环:
+app.mainloop()
